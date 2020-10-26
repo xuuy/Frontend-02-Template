fcf0dd97d54f32aa334d3faba1e4d7ddaa3e9628
Mack <17602124574@163.com>
2020-10-26 10:24:47 +0800

    update note

---
+++ week16/generators/templates/webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require("webpack"); //to access built-in plugins
+const CopyPlugin = require("copy-webpack-plugin");
+// webpack.config.js
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  module: {
+    rules: [
+      { test: /\.vue$/, use: "vue-loader" },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src/*.html", to: "[name].[ext]" }],
+    }),
+  ],
+};
